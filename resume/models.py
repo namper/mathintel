6e487146091f4d53ac667de9f7c1b4a252f1a344
Misho <nnamper@gmail.com>
2020-10-01 12:52:12 +0400

    SofDeletion Implementation For `resume` models

--- resume/models.py
+++ resume/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from resume.utils import SoftDeletionModel
 
 
-class Field(models.Model):
+class Field(SoftDeletionModel):
     resume = models.ForeignKey('Resume', related_name='fields', on_delete=models.PROTECT)
 
 
-class Resume(models.Model):
+class Resume(SoftDeletionModel):
     owner = models.ForeignKey(to='user.User', null=True, on_delete=models.CASCADE)
